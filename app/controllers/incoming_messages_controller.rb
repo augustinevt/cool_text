a228fd7eeeffc48a937168a426c0d62c2cef9496
August von Trapp and Brett New <Guest@epicodus-44.local>
2016-09-27 15:13:23 -0700

    user can get news and space image depending on what message they text

--- app/controllers/incoming_messages_controller.rb
+++ app/controllers/incoming_messages_controller.rb
@@ -1,13 +1,16 @@
 class IncomingMessagesController < ApplicationController
   protect_from_forgery except: :create
+
   def create
     status = 'init'
-    im = IncomingMessage.new(params[:Body], params[:To], params[:From])
+    im = IncomingMessage.new(params[:Body], params[:To], params[:From], params[:MediaUrl])
 
-    if im.respond_with("I read no texts...")
-      status = 'message sent'
+    if im.body === 'space'
+      im.respond_with(im.scrape_space)
+    elsif im.body === 'news'
+      im.respond_with(im.print_headlines(im.scrape_news))
     else
-      status = 'message not sent'
+      im.respond_with('Foo foo, you are...')
     end
   end
 
--- app/models/incoming_message.rb
+++ app/models/incoming_message.rb
@@ -1,24 +1,25 @@
 class IncomingMessage
 
-  def initialize (body, to, from)
+  def initialize (body, to, from, media)
    @body = body
    @to = to
    @from = from
+   @media = media
   end
 
   attr_accessor :body, :to, :from
 
 
-  def respond_with(message)
-    new_message = print_headlines(scrape)
-    binding.pry
+  def respond_with(body)
+    payload = {Body: body[0], To: @from, From: @to}
+    payload[:MediaUrl] = body[1] if body[1]
     begin
      response = RestClient::Request.new(
      method: :post,
      url: "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
      user: ENV['TWILIO_ACCOUNT_SID'],
      password: ENV['TWILIO_AUTH_TOKEN'],
-     payload: {Body: new_message, To: @from, From: @to}
+     payload: payload
      ).execute
    rescue
 
@@ -28,7 +29,7 @@ class IncomingMessage
 
 
 
-  def scrape
+  def scrape_news
     require 'open-uri'
     doc = Nokogiri::HTML(open('http://www.nytimes.com/'))
     headlines = doc.css('article')
@@ -43,12 +44,20 @@ class IncomingMessage
     return headlinesArray[0..4]
   end
 
+  def scrape_space
+    require 'open-uri'
+    require 'uri'
+    doc = Nokogiri::HTML(open('http://www.astronomy.com/photos/picture-of-day'))
+    image_path = URI.encode('http://www.astronomy.com' + doc.css('.hero').css('img')[0]['src'])
+    [nil, image_path]
+  end
+
   def print_headlines(headlinesArray)
     full_message = "Here is the news today: \n\n\n"
     headlinesArray.each do |headline|
       full_message += headline.title + "\n\n" + headline.summary + "\n\n"
     end
-    return full_message
+    return [full_message, nil]
   end
 
 
